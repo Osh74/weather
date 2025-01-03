cd4bbac3e46225f20e494ab87cbe0bed329800d9
Osher <osh31310@gmail.com>
2025-01-03 11:01:12 +0200

    weather - project code

--- main.py
+++ main.py
@@ -1,1 +1,41 @@
-print("my project is very very amazing")
+import requests
+import json
+
+api_key='a62b27f77c9f45ecab8123608242812'
+
+city_name=input("Please enter a city name for getting the current weather in it: ")
+
+def weatherֹ_by_city(city_name, api_key):
+
+    #Base + complete url for the API request
+    base_url="http://api.weatherapi.com/v1/current.json?"
+    complete_url=base_url + "key=" + api_key + "&q=" + city_name
+
+    #Get request to the API
+    response=requests.get(complete_url)
+
+    #Converting to JSON
+    data=response.json()
+
+    #Erxtacrting data if city was found
+    if "error" not in data:
+        current=data["current"]
+        location=data["location"]
+
+        #Extracting relevant fields
+        temperature=current["temp_c"]
+        condition=current["condition"]["text"]
+        humidity=current["humidity"]
+        wind_speed=current["wind_kph"]
+
+        # Display the weather data
+        print(f"Weather in {location['name']}, {location['country']}:")
+        print(f"Temperature: {temperature}°C")
+        print(f"Description: {condition}")
+        print(f"Humidity: {humidity}%")
+        print(f"Wind speed: {wind_speed} kph")
+    else:
+        print(f"City '{city_name}' not found.")
+
+# Call the function to get weather data
+weatherֹ_by_city(city_name, api_key)
